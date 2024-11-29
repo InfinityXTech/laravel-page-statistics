1864ce8a472f67e239d2e9285486f3772e821ba4
Developer <developer@infinity-x.tech>
2024-11-29 06:59:28 +0100

    cleanup

--- src/PageStatistics.php
+++ src/PageStatistics.php
@@ -7,48 +7,55 @@ use GeoIp2\Database\Reader;
 use Illuminate\Database\Eloquent\Model;
 
 class PageStatistics {
-    public function getDetails () {
-        // Get the user agent and IP address
-        $userAgent = request()->userAgent();
-        $ipAddress = request()->ip();
-
+    public function getDeviceData () {
         // Device Detection
+        $userAgent = request()->userAgent();
+
         $deviceDetector = new DeviceDetector($userAgent);
         $deviceDetector->parse();
         $deviceType = $deviceDetector->getDeviceName(); // 'phone', 'tablet', 'desktop', or 'other'
         $deviceData = $deviceDetector->getDevice(); // All device data
         $clientInfo = $deviceDetector->getClient(); // holds information about browser, feed reader, media player, ...
         $osInfo = $deviceDetector->getOs();
+        
+        return [
+            'device' => [
+                'name' => $deviceType,
+                'data' => $deviceData
+            ],
+            'os' => $osInfo,
+            'client' => $clientInfo,
+        ];
+    }
 
-        if (!in_array($ipAddress, ['127.0.0.1', '::1'])) {
-            // IP Geolocation
-            $reader = new Reader(config('laravel-page-statistics.geoip_db_path', resource_path('data/geo.mmdb')));
+    public function getLocationData () {
+        $ipAddress = request()->ip();
+
+        if (!in_array($ipAddress, ['127.0.0.1', '::1']) && $geoIpDB = config('laravel-page-statistics.geoip_db_path')) {
+
+            $reader = new Reader($geoIpDB);
             $geoInfo = $reader->city($ipAddress);
             $countryName = $geoInfo->country->name;
             $cityName = $geoInfo->city->name;
-            $countryCode = $geoInfo->country->isoCode; // 'CA' for Canada, etc.
+            $countryCode = $geoInfo->country->isoCode;
 
-            // Prepare location and meta information
-
-            $locationMeta = [
+            return [
                 'country' => $countryName,
                 'city' => $cityName,
                 'short_code' => $countryCode,
                 'ip_address' => $ipAddress,
             ];
 
-            $deviceMeta = [
-                'name' => $deviceType,
-                'data' => $deviceData
-            ];
-
-            return [
-                'location' => $locationMeta,
-                'device' => $deviceMeta,
-                'os' => $osInfo,
-                'client' => $clientInfo,
-            ];
         }
+    }
+
+    public function getDetails () {
+
+        $data = $this->getDeviceData();
+
+        $data['location'] = $this->getLocationData();
+
+        return $data;
     }
 
     public function record (Model $model, $data = []) {
